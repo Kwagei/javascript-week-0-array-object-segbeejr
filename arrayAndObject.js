3dca5c0d6270f567a525055e2bb7ae1a5c7488fa
N. Omalley Segbee Jr <segbeejr67@gmail.com>
2023-06-06 22:53:01 +0000

    Complete Assignment

---
+++ arrayAndObject.js
@@ -0,0 +1,103 @@
+
+    // 1. declare an array with 5 items.
+
+        // Answer:
+        let arr = ["His Exelency", "Rev. Dr. Omalley Segbee", "President, Republic of Liberia", 2029, "Executive Massion"]
+
+    // 2. add an item to the array
+
+        // Answer:
+        arr[6] = "By God's Grace"
+
+    // 3. update the third item in the array
+
+        // Answer:
+        arr[2] = "President of the Wonderfully made and blessed Republic of Liberia"
+
+
+
+    // let arr = [0,1,2,3,4,[5,6,7],8,9]
+    // 4. print 5 from the array define above
+
+        // Answer:
+        arr[5][0]
+
+
+    // 6. print the last item in the array above using the array length property
+
+        // Answer:
+        arr[arr.length -1]
+
+
+
+    // 7. write a program that swaps 0 and 1 in the array and produces this output [1,0,2,3,4,[5,6,7],8,9]
+
+        // Answer:
+        arr[0] = 1;
+        arr[1] = 0;
+
+
+
+    // 8. declear an object call class with 3 properties
+
+        // Answer:
+        let object = {
+            name: "Omalley",
+            height: "5'10"
+            class: "Cohort 3"
+        }
+
+
+
+    // 9. add a new property to the object
+
+        // Answer:
+        object["school"] = "KIT"
+
+
+    // 10. print the first item from the object using the dot notation
+
+        // Answer:
+        object.name
+
+
+    // 11. print the last item from the object using the square bracket notation 
+
+        // Answer:
+        object["class"]
+    
+    
+    // let people = [
+    //      {name: "mary", contact: 0881999000},
+    //      {name: "paul", contact: 0881999600},
+    //      {name: "peter", contact: 0881999400},
+    //      {name: "sara", contact: 0881999300}]
+
+    // 12. print mary from the list of people
+
+        // Answer:
+        people[0].name
+
+
+    // 13. add gender to each of the object in the array
+
+        // Answer:
+        people[0].gender = "female"
+        people[1].gender = "male"
+        people[2].gender = "male"
+        people[3].gender = "female"
+
+
+
+    // 14. add a new object to the list of people
+
+        // Answer:
+        people[people.length] = {name: "Nathan", contact: 0888899188}
+
+
+
+    // 15. print sara's contact 
+        
+        // Answer:
+        people[3].contact
+
